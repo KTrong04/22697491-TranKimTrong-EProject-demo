723f2f1a452b5d04ceade1cae7b7ab487f41ccb4
Tran Kim Trong <KTrongw2@gmail.com>
2025-10-23 23:42:38 +0700

    FIX PRO MAX YAA

--- order/src/config.js
+++ order/src/config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-    mongoURI: process.env.MONGODB_ORDER_URI || 'mongodb://localhost/orders',
-    rabbitMQURI: process.env.RABBITMQ_URI || 'amqp://localhost',
+    mongoURI: process.env.MONGODB_ORDER_URI || "mongodb://localhost/orders",
+    rabbitMQURI: process.env.RABBITMQ_URI || "amqp://127.0.0.1:5672",
     rabbitMQQueue: 'orders',
     port: 3002
 };
